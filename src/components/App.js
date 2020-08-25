f0b48ca14b2d1a40a91a9f7b718d3646fad65e64
LenaIT <olena.denysova@gmail.com>
2020-08-25 11:38:08 +0200

    connected css

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import "./css/App.scss";
+import "../css/App.scss";
 
 
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <h1>Hello World!</h1>
     </div>
   );
 }
