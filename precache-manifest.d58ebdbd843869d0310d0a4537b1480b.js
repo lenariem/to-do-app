c1694f57b6a3d3fbd2db38f1649d126740f6801b
LenaIT <olena.denysova@gmail.com>
2020-09-01 09:48:56 +0200

    Updates

---
+++ precache-manifest.d58ebdbd843869d0310d0a4537b1480b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1b24f80a48e293bcacce8e690d6e3e",
+    "url": "/lenariem/to-do-app/index.html"
+  },
+  {
+    "revision": "d47048e5d09a296316e1",
+    "url": "/lenariem/to-do-app/static/css/main.5d67a9a3.chunk.css"
+  },
+  {
+    "revision": "ca1ac7e3fe3f563cfcd4",
+    "url": "/lenariem/to-do-app/static/js/2.3ec21336.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lenariem/to-do-app/static/js/2.3ec21336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47048e5d09a296316e1",
+    "url": "/lenariem/to-do-app/static/js/main.2b067b19.chunk.js"
+  },
+  {
+    "revision": "8b5574ca9bfab2bc6f4a",
+    "url": "/lenariem/to-do-app/static/js/runtime-main.8b69f9a7.js"
+  }
+]);
